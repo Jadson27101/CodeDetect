36ea24a9c53ef9975a7c60fa2926d693c2e0b604
Vlad Frolenko <jonny_walker@mail.ru>
2018-03-19 09:18:16 -0700

    First commit

---
+++ app/src/main/java/com/example/jadso/codedetect/MainActivity.java
@@ -0,0 +1,183 @@
+package com.example.jadso.codedetect;
+
+import android.annotation.SuppressLint;
+import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.net.Uri;
+import android.os.Environment;
+import android.os.StrictMode;
+import android.provider.MediaStore;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.util.Log;
+import android.util.SparseArray;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ImageView;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import com.google.android.gms.vision.Frame;
+import com.google.android.gms.vision.barcode.Barcode;
+import com.google.android.gms.vision.barcode.BarcodeDetector;
+
+import java.io.File;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+
+import static android.provider.MediaStore.Files.FileColumns.MEDIA_TYPE_VIDEO;
+
+public class MainActivity extends AppCompatActivity {
+    TextView scannedMessageView;
+    private static final int MEDIA_TYPE_IMAGE = 1;
+    private Uri fileUri;
+    private ImageView myimageView;
+    private static final int CAMERA_CAPTURE_IMAGE_REQUEST_CODE = 100;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        Button scan = (Button) findViewById(R.id.scan);
+        Button clear = (Button) findViewById(R.id.clear);
+        myimageView = (ImageView) findViewById(R.id.scannedImage);
+        scannedMessageView = (TextView) findViewById(R.id.scannedMessage);
+        getIcon();
+        scan.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+                captureImage();
+            }
+
+        });
+        clear.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+                scannedMessageView.setText("");
+            }
+
+        });
+    }
+    private void getIcon(){
+        myimageView.setImageBitmap(BitmapFactory.decodeResource(this.getResources(), R.drawable.icon));
+    }
+
+    //    View.OnClickListener viewOnClickListener = new View.OnClickListener() {
+//        @Override
+//        public void onClick(View v) {
+//            switch (v.getId()){
+//                case R.id.scan:
+//                    //scanQR();
+//                    //isDeviceSupportCamera();
+//                    captureImage();
+//                    break;
+//                case R.id.clear:
+//                    scannedMessageView.setText("");
+//                    break;
+//            }
+//        }
+//    };
+    private boolean isDeviceSupportCamera() {
+        if (getApplicationContext().getPackageManager().hasSystemFeature(
+                PackageManager.FEATURE_CAMERA)) {
+            scannedMessageView.setText("Found camera!!1");
+            // this device has a camera
+            return true;
+        } else {
+            // no camera on this device
+            return false;
+        }
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        // if the result is capturing Image
+        if (requestCode == CAMERA_CAPTURE_IMAGE_REQUEST_CODE) {
+            if (resultCode == RESULT_OK) {
+                // successfully captured the image
+                // display it in image view
+                previewCapturedImage();
+            } else if (resultCode == RESULT_CANCELED) {
+                // user cancelled Image capture
+                Toast.makeText(getApplicationContext(),
+                        "User cancelled image capture", Toast.LENGTH_SHORT)
+                        .show();
+            } else {
+                // failed to capture image
+                Toast.makeText(getApplicationContext(),
+                        "Sorry! Failed to capture image", Toast.LENGTH_SHORT)
+                        .show();
+            }
+        }
+    }
+
+    private static File getOutputMediaFile(int type) {
+        File mediaStorageDir = new File(Environment.getExternalStoragePublicDirectory(
+                Environment.DIRECTORY_PICTURES), "MyCameraApp");
+        if (!mediaStorageDir.exists()) {
+            if (!mediaStorageDir.mkdirs()) {
+                Log.d("MyCameraApp", "failed to create directory");
+                return null;
+            }
+        }
+        // Create a media file name
+        @SuppressLint("SimpleDateFormat") String timeStamp = new SimpleDateFormat("yyyyMMdd_HHmmss").format(new Date());
+        File mediaFile;
+        if (type == MEDIA_TYPE_IMAGE) {
+            mediaFile = new File(mediaStorageDir.getPath() + File.separator +
+                    "IMG_" + timeStamp + ".jpg");
+        } else {
+            return null;
+        }
+
+        return mediaFile;
+    }
+
+    private static Uri getOutputMediaFileUri(int type) {
+        return Uri.fromFile(getOutputMediaFile(type));
+    }
+
+    private void captureImage() {
+        StrictMode.VmPolicy.Builder builder = new StrictMode.VmPolicy.Builder();
+        StrictMode.setVmPolicy(builder.build());
+        Intent intent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
+        fileUri = getOutputMediaFileUri(MEDIA_TYPE_IMAGE);
+        intent.putExtra(MediaStore.EXTRA_OUTPUT, fileUri);
+        // start the image capture Intent
+        startActivityForResult(intent, CAMERA_CAPTURE_IMAGE_REQUEST_CODE);
+    }
+
+    private void previewCapturedImage() {
+        try {
+            myimageView.setVisibility(View.VISIBLE);
+            BitmapFactory.Options options = new BitmapFactory.Options();
+            options.inSampleSize = 8;
+            final Bitmap bitmap = BitmapFactory.decodeFile(fileUri.getPath(), options);
+            myimageView.setImageBitmap(bitmap);
+            scanQR(bitmap);
+        } catch (NullPointerException e) {
+            e.printStackTrace();
+        }
+    }
+    private void scanQR(Bitmap bitmap) {
+        BarcodeDetector barcodeDetector = new BarcodeDetector.Builder(getApplicationContext())
+                .setBarcodeFormats(Barcode.ALL_FORMATS)
+                .build();
+        if (!barcodeDetector.isOperational()) {
+            scannedMessageView.setText("Error! Please check your Internet connection!");
+        } else {
+            try {
+                Frame frame = new Frame.Builder().setBitmap(bitmap).build();
+                SparseArray<Barcode> barcodes = barcodeDetector.detect(frame);
+                if (barcodes.size()==0){
+                    scannedMessageView.setText("Error, QR not found!");
+                }else {
+                    Barcode thisCode = barcodes.valueAt(0);
+                    scannedMessageView.setText(thisCode.rawValue);
+                }
+                } catch (NullPointerException e) {
+                e.printStackTrace();
+            }
+        }
+    }
+}
